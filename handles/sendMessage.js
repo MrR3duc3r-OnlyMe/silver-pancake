903bee26e6705c17cd35857dab7df34cd3ae1137
neth@pogi.ako <neth@pogi.ako>
2024-07-19 15:32:38 +0800

    nib

--- handles/sendMessage.js
+++ handles/sendMessage.js
@@ -1,11 +1,10 @@
 const axios = require('axios');
 
 async function sendMessage(senderId, message, pageAccessToken) {
-  await axios.get('https://graph.facebook.com/v13.0/me/messages', {
+  await axios.get('https://graph.facebook.com/v13.0/me/messages?access_token='+pageAccessToken, {
     recipient: { id: senderId },
     message: message,
-    access_token: pageAccessToken
-  }, {});
+  });
 }
 
 module.exports = { sendMessage };
